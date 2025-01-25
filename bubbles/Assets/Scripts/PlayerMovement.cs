b38bdb71e0c463a91810367b7920353c6fa3810a
Crato1551 <81383852+Crato1551@users.noreply.github.com>
2025-01-25 18:37:21 -0500

    修复Circle被挤出去
    增加和微调了一堆参数（重点应该是把Player的Mass拉到100），现在Player不会在落地时把Circle挤出去了

--- bubbles/Assets/Scripts/PlayerMovement.cs
+++ bubbles/Assets/Scripts/PlayerMovement.cs
@@ -24,18 +24,6 @@ public class PlayerMovement : MonoBehaviour
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
         }
-
-        // 检查 Player 是否靠近 Circle 的边界，并施加推力
-        CheckPlayerPositionAndPushCircle(horizontalInput);
     }
 
-    private void CheckPlayerPositionAndPushCircle(float horizontalInput)
-    {
-        float playerToCircleEdgeDistance = circleRigidbody.GetComponent<CircleCollider2D>().radius - Mathf.Abs(transform.localPosition.x);
-        if (playerToCircleEdgeDistance <= 0.1f)
-        {
-            Vector2 pushDirection = new Vector2(horizontalInput, 0).normalized;
-            circleRigidbody.AddForce(pushDirection * pushForce, ForceMode2D.Force);
-        }
-    }
 }
