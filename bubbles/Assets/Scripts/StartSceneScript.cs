53bc202eb135f89f170bcc1aaf73763dda81b79c
Crato1551 <81383852+Crato1551@users.noreply.github.com>
2025-01-26 14:03:12 -0500

    开始UI
    ✌

---
+++ bubbles/Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // 加载游戏场景，替换 "GameScene" 为你的实际场景名称
+        SceneManager.LoadScene("SampleScene");
+    }
+}
